c5a9032743d859be53121f433dd0bddad002afc5
Cristian Ceban <cristian.ceban@etu.u-pec.fr>
2020-02-18 15:44:41 +0200

    add shell.c

--- shell.c
+++ shell.c
@@ -3,30 +3,45 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+#include <signal.h>
 
 #define MAX 1024
-static char line[1024];
+static char line[MAX];
+char *var;
+char *val;
 
 void affiche_cmd(char *argv[]);
-int parse_line(char *s,char **argv[]);
+int parse_line(char *s, char **argv[]);
 void print_dir();
 int takeInput(char* str);
-
-int main(int argc,char *argv[])
+void simple_cmd(char *str);
+int parse_line_redir (char * s, char **argv[], char **in, char **out);
+int redir_cmd(char*argv[],char*in,char*out);
+int parse_line_pipes(char*s,char***argv[],char**in,char**out);
+void sighandler(int sig_num);
+int main(int argc, char *argv[])
 {
+	
+	char *in,*out;
 	char option;
-	char inputString[MAX];
-		while(1){
-			print_dir();
-			fflush(NULL);
- 
-			if (!fgets(line, 1024, stdin)) 
-				return 0;
- 
-		}
+	while(1)
+	{
+		signal(SIGTSTP, sighandler); 
+		print_dir();
+		fflush(NULL);
 
+		if (!fgets(line, MAX, stdin)) 
+			return 0;
+		char *cmd = line;
+		parse_line(cmd,&argv);
+		if (var!=NULL && val!= NULL && strstr(cmd,var)!=NULL)
+			printf("%s",val);
+		redir_cmd(&cmd,in,out);
+	}
+	
+	//parse_line_pipes("cmd1 toto|cmd2 tata titi|cmd3",&argv,&in,&out);
+	//parse_line("chaine=valeur",&argv);
+	//parse_line_redir("command -v <input >output",&argv,&in,&out);
 	return 0;
 }
 
@@ -46,46 +61,137 @@ void print_dir(){
     printf("\n%s$ ", cwd); 
 }
 
-/*int parse_line(char *s,char **argv[]){
-	char key[]= " ";
-	char *pch = strpbrk(s,key);
-	int sizen=100;
-	int sizec=1024;
-	char temp[sizen][sizec];
-	int i = 0;
-	int nr_col=0;
-	while(pch!=NULL){
-		int j = 0;
-		while(s!=pch){
-			if(*s==' ')
-			s++;
-			temp[i][j]=*s;
-			s++;
-			j++;
-		} 
-		i++;
-		pch = strpbrk (pch+1,key);
+
+void simple_cmd(char *str){
+	if (strstr(str, "exit") != NULL) {
+		exit(0);
 	}
-	int j = 0;
-	while(*s!='\0'){
-		if(*s==' ')
-		s++;
-		temp[i][j]=*s;
-		j++;
-		s++;
+	else if (strstr(str, "cd") != NULL){
+		chdir("..");
 	}
-	i++;
-	printf("\n");
-	for(int k = 0;k<i;k++){
-		int j = 0;
-		while(temp[k][j]!='\0'){
-			printf("%c",temp[k][j]);
-			j++;
+	else if (strstr(str, "./" )!= NULL){
+		system(str);
+	}
+	else if (strstr(str, "ls")!=NULL){
+		system(str);
+	}
+	else if (strstr(str,"$"!=NULL && var!=NULL && val!=NULL)){
+		system(*val);
+	}
+}
+
+int parse_line(char *s, char **argv[]){
+	if(strstr(s,"=")!=NULL){
+		char key[] = "=";
+		char *temp = strdup(s);
+		char *token = strtok(temp,key);
+		int i = 0;
+		while(token != NULL){
+			if(i == 0){
+				var =(char*)malloc(sizeof(char)*strlen(token)+1);
+				var = token;
+				i++;
+			}
+			else if(i == 1){
+				val = (char*)malloc(sizeof(char)*strlen(token)+1);
+				val = token;
+			}
+			token = strtok(NULL,key);
 		}
-		printf("\n");
+		(*argv)[i] = NULL;
+		//affiche_cmd(*argv);
 	}
-	temp[i][0]=NULL;
-	//	affiche_cmd(temp);
+	else{
+	char key[] = " ";
+	char *temp = strdup(s);
+	char *token = strtok(temp,key);
+	int i = 0;
+	while(token != NULL){
+		(*argv)[i] = malloc(sizeof(char*)*(strlen(temp)+1));
+		(*argv)[i++] = token;
+		token = strtok(NULL,key);
+	}
+	(*argv)[i] = NULL;
 	//affiche_cmd(*argv);
+	}
 	return 0;
-}*/
+}
+
+int parse_line_redir (char *s, char **argv[], char **in, char **out){
+	char key[] = " ";
+	char *temp = strdup(s);
+	char *token = strtok(temp,key);
+	int i = 0;
+	while(token != NULL){
+		if(strstr(token,"<") != NULL){
+			*in =(char*)malloc(sizeof(char)*(strlen(token)+1));
+			char *temp1 = token;
+			temp1++;
+			*in=temp1;
+		}
+		else if(strstr(token,">") != NULL){
+			*out =(char*)malloc(sizeof(char)*(strlen(token)+1));
+			char *temp1 = token;
+			temp1++;
+			*out = temp1;
+		}
+		(*argv)[i] = malloc(sizeof(char*)*(strlen(token)+1));
+		(*argv)[i++] = token;
+		token = strtok(NULL,key);
+	}
+	(*argv)[i] = NULL;
+	affiche_cmd(*argv);
+	return 0;
+}
+
+int redir_cmd(char*argv[],char*in,char*out){
+	simple_cmd(*argv);
+}
+
+int parse_line_pipes(char*s,char***argv[],char**in,char**out){
+	char key[] = "|";
+	char *temp = strdup(s);
+	char *token = strtok(temp,key);
+	int count = 0;
+	int i = 0;
+	while(s[i]!=NULL){
+		if (s[i]=='|')
+		count++;
+		i++;
+	}
+	i = 0;
+	char **tempPipe = (char**)malloc(sizeof(char*)*(count+1));
+	while(token != NULL){
+		tempPipe[i] = (char*)malloc(sizeof(char)*(strlen(token)+1));
+		tempPipe[i++] = token;
+		token = strtok(NULL,key);
+	}
+	tempPipe[i]=NULL;
+	**argv=malloc(sizeof(char**)*(count+1));
+	key[0] = ' ';
+	for(int j = 0;j<=count;j++){
+		char *temp1 = strdup(tempPipe[j]);
+		token = strtok(temp1,key);
+		int k = 0;
+		count = 0;
+		int it = 0;
+		while(tempPipe[j][it]!=NULL){
+			if (tempPipe[j][it++]==' ')
+				count++;
+		}
+		**argv[j]=malloc(sizeof(char**)*(count + 1));
+		while(token != NULL){
+			**argv[k] = malloc(sizeof(char*)*(strlen(temp1)+1));
+			**argv[k++] = token;
+			token = strtok(NULL,key);
+		}
+	}
+	(**argv)[i] = NULL;
+	return 0;
+}
+
+void sighandler(int sig_num) 
+{ 
+    signal(SIGTSTP, sighandler); 
+    printf("Cannot execute Ctrl+Z\n"); 
+} 
